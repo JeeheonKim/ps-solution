86d782fb653dc7fd05f192bb7ed19a84fa5513d5
Jeeheon Kim <jeeheonkim0@gmail.com>
2020-01-17 14:00:22 -0500

    laptop

--- etc/mergeSort.cpp
+++ etc/mergeSort.cpp
@@ -4,45 +4,55 @@
 #include <vector>
 using namespace std;
 
-int merge(vector<int>& arr, int start, int mid, int end){
-    int a=0, b=mid+1, i=0;
-    int tmp[50000];
-    while (a<mid && b<end){
-        if (arr[a] < arr[b]) {
-            tmp[i++] = arr[a++];
-        } else if (arr[b] < arr[a]) {
-            tmp[i++] = arr[b++];
-        } else {
-            tmp[i++] = arr[a++];
-            tmp[i++] = arr[b++];
-        }
-    }
-
-    if (a>=mid) {
-        while (b<end){
-            tmp[i++] = arr[b++];
-        }
-    }
-
-    i=0;
-    for (auto& a: arr)
-        a = tmp[i++] ;
-}
-
-int mergeSort(vector<int>& arr, int start, int end){
-    if (start<end){
-        int mid = (start-end)/2;
-        mergeSort(arr, start, mid);
-        mergeSort(arr, mid+1, end);
-        merge(arr, start, end, mid);
-        return 0;
-    }
-}
-
 int print(vector<int> v) {
     for (auto a : v){
         cout << a << " ";
     }
+    cout << '\n';
+}
+
+int merge(vector<int>& arr, int start, int mid, int end){
+    int a=start, b=mid+1, i=0;
+    int tmp[1000];
+
+    while (a<=mid && b<=end){
+        if (arr[a] <= arr[b]) {
+            tmp[i++] = arr[a++];
+        } else if (arr[b] < arr[a]) {
+            tmp[i++] = arr[b++];
+        } 
+        else {
+            tmp[i++] = arr[a++];
+            tmp[i++] = arr[b++];
+        }
+    }
+
+    if (a>mid) { //first array is empty
+        while (b<=end){
+            tmp[i++] = arr[b++];
+        }
+    } else {
+        while (a<=mid){
+            tmp[i++] = arr[a++];
+        }
+    }
+
+    i=0;
+    for (int j=start; j<=end; ++j)
+        arr[j] = tmp[i++];
+        
+    return 0;
+}
+
+int mergeSort(vector<int>& arr, int start, int end){
+    int mid = (start+end)/2;
+
+    if (start<end){
+        mergeSort(arr, start, mid);
+        mergeSort(arr, mid+1, end);
+        merge(arr, start, mid, end);
+    }
+    return 0;
 }
 
 int main(){
